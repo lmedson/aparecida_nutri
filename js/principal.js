6e63cb40f539125790b4e796f5b70297eea86929
lmedson <medson-mbp@hotmail.com>
2018-04-04 13:32:12 -0300

    #04: A03 - Praticando com estilos

--- js/principal.js
+++ js/principal.js
@@ -1,38 +1,46 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro_paciente");
-
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-
-var tdAltura = 	paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-
-var tdImc = paciente.querySelector(".info-imc");
-
-var pesoEhValido = true;
-var alturaEhValida = true;
+var pacientes = document.querySelectorAll(".paciente");
 
 
-if (peso <= 0 || peso >= 1000) {
-	console.log("peso inválido");
-	pesoEhValido = false;
-	tdImc.textContent = "Peso inválido";
+for (var i = 0 ; i < pacientes.length ; i++) {
+
+
+	var paciente = pacientes[i];
+
+	var tdPeso = paciente.querySelector(".info-peso");
+	var peso = tdPeso.textContent;
+
+	var tdAltura = 	paciente.querySelector(".info-altura");
+	var altura = tdAltura.textContent;
+
+	var tdImc = paciente.querySelector(".info-imc");
+
+	var pesoEhValido = true;
+	var alturaEhValida = true;
+
+
+	if (peso <= 0 || peso >= 1000) {
+		console.log("peso inválido");
+		pesoEhValido = false;
+		tdImc.textContent = "Peso inválido";
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if (altura < 0 || altura >3.00 ){
+		console.log("Altura inválida");
+		alturaEhValida = false;
+		tdImc.textContent = "Altura inválida";
+		paciente.classList.add("paciente-invalido");
+		//paciente.style.backgroundColor = "lightcoral";
+	}
+
+	if( alturaEhValida && pesoEhValido){		
+		var imc = peso /( altura * altura);
+		tdImc.textContent = imc.toFixed(2);
+
+	}
+
+	console.log(paciente.textContent);//tr
 }
-
-if (altura < 0 || altura >3.00 ){
-	console.log("Altura inválida");
-	alturaEhValida = false;
-	tdImc.textContent = "Altura inválida";
-
-}
-
-if( alturaEhValida && pesoEhValido){		
-	var imc = peso /( altura * altura);
-	tdImc.textContent = imc;
-
-}
-
-console.log(paciente.textContent);//tr
-//console.log(imc);//td com o peso
