94c8757706a3a0bf0895453aeee3ab3343a00d81
lmedson <medson-mbp@hotmail.com>
2018-04-09 01:38:23 -0300

    #11: A09.2 tratando request

--- js/form.js
+++ js/form.js
@@ -5,11 +5,9 @@ botaoAdicionar.addEventListener("click", function(event) {
     var form = document.querySelector("#form-adiciona");
     var paciente = obtemPacienteDoFormulario(form);
 
-
-	var pacienteTr = montaTr(paciente);
-
 	var erros = validaPaciente(paciente);
 	console.log(erros);
+
 	if(erros.length > 0){
 		exibeMensagensDeErro(erros);
 
@@ -20,14 +18,20 @@ botaoAdicionar.addEventListener("click", function(event) {
 		console.log("Paciente Inválido");
 		return;
 	}
-	var tabela = document.querySelector("#tabela-pacientes");
-	tabela.appendChild(pacienteTr);
+
+	adicionaPacienteNaTabela(paciente);
 
 	form.reset();
 	var mensagemsErro = document.querySelector("#mensagens-erro");
 	mensagemsErro	.innerHTML = "";
 
 });
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
 
 function exibeMensagensDeErro(erros){
 	var ul = document.querySelector("#mensagens-erro");
